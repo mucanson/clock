581db92172f2a3a92229a40b7fedc5b407b31790
mucanson <truonglovewho@gmail.com>
2020-02-15 20:43:13 +0700

    first commit

--- lcd.ino
+++ lcd.ino
@@ -4,7 +4,7 @@
 #include <LiquidCrystal.h>
 #define DHTTYPE DHT11
 #define DHTPIN 8
-#define button 7
+//#define button 7
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 RTC_DS1307 rtc;
@@ -12,12 +12,12 @@ DHT dht(DHTPIN, DHTTYPE);
 float temp, humid;
 int state = 0;
 int Laststate = 0;
-int count = 0,count1=0;
+int count = 0, count1 = 0;
 char daysOfTheWeek[7][12] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
 void setup() {
   lcd.begin(16, 2);
   dht.begin();
-  pinMode(button, INPUT);
+  //pinMode(button, INPUT);
   while (!Serial);
   Serial.begin(57600);
   if (! rtc.begin()) {
@@ -28,28 +28,16 @@ void setup() {
     Serial.println("RTC is NOT running!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
-  DisplayTime();
 }
 
 void loop() {
-  DisplayTime();
-  state = digitalRead(button);
-  if (state != Laststate) {
-    if (state == HIGH) {
-      count++;
-    }
-    Laststate = state;
+  if (millis() % 5000 == 0)
+  {
     lcd.clear();
-    if (count % 2 == 0)
-    {
-      lcd.clear();
-      DisplayTime();
-    }
-    else {
-      lcd.clear();
-      DisplayTemp();
-    }
+    DisplayTemp();
   }
+  lcd.clear();
+  DisplayTime();
 }
 void DisplayTime() {
   DateTime now = rtc.now();
